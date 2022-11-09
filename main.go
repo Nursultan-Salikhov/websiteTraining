6006bfa593a4913372b232ff8a5abaa31e6509ef
Nursultan-Salikhov <84440114+Nursultan-Salikhov@users.noreply.github.com>
2022-11-09 17:30:42 +0300

    Added html file for homePage. Practiced with the display of data on the page.

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type User struct {
+	Name                  string
+	Age                   uint8
+	Money                 int16
+	AvgGrades, HappyLevel float64
+	Hobbies               []string
+}
+
+func init() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/contacts", contactsPage)
+	http.ListenAndServe(":8080", nil)
+}
+
 func main() {
 
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	bob := User{"Bob", 26, 200, 0.9, 0.25, []string{"Football", "Reading", "TV"}}
+	tmpl, _ := template.ParseFiles("templates/homePage.html")
+	tmpl.Execute(w, bob)
+}
+
+func contactsPage(w http.ResponseWriter, r *http.Request) {
+	bob := User{}
+	bob.Name = "Alex"
+	fmt.Fprintf(w, bob.getAllInfo())
+	//tmpl, _ := template.ParseFiles("templates/contactsPage.html")
+}
+
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is %s, \nAge - %d, Money -%d, AvdGraduate - %f, Happy level -%f", u.Name, u.Age, u.Money, u.AvgGrades, u.HappyLevel)
 }
