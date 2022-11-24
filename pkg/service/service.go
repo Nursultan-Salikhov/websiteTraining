5463f88342236ceb0b5d87274dd0cce181f9e662
Nursultan-Salikhov <84440114+Nursultan-Salikhov@users.noreply.github.com>
2022-11-24 13:56:54 +0300

    Didn't notice that not all files were added

---
+++ pkg/service/service.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	todo "websiteTraining"
+	"websiteTraining/pkg/repository"
+)
+
+type Authorization interface {
+	CreateUser(user todo.User) (int, error)
+	GenerateToken(username, password string) (string, error)
+	ParseToken(accessToken string) (int, error)
+}
+
+type TodoList interface {
+	Create(userId int, list todo.TodoList) (int, error)
+	GetAll(userId int) ([]todo.TodoList, error)
+	GetById(userId, listId int) (todo.TodoList, error)
+	Delete(userId, listId int) error
+	Update(userId, listId int, input todo.UpdateListInput) error
+}
+
+type TodoItem interface {
+	Create(userId, listId int, item todo.TodoItem) (int, error)
+	GetAll(userId, listId int) ([]todo.TodoItem, error)
+	GetById(userId, itemId int) (todo.TodoItem, error)
+	Delete(userId, itemId int) error
+	Update(userId, itemId int, input todo.UpdateItemInput) error
+}
+
+type Service struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewService(rep *repository.Repository) *Service {
+	return &Service{
+		Authorization: NewAuthService(rep.Authorization),
+		TodoList:      NewTodoListService(rep.TodoList),
+		TodoItem:      NewTodoItemService(rep.TodoItem, rep.TodoList),
+	}
+}
